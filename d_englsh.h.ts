f65194d5b54d4ace844890da6f7be82ccc2e208f
XANADryden <46723459+XANADryden@users.noreply.github.com>
2020-09-21 14:00:34 -0500

    Update d_englsh.h.ts

--- d_englsh.h.ts
+++ d_englsh.h.ts
@@ -1,26 +1,26 @@
 // Emacs style mode select   -*- C++ -*- 
-  //-----------------------------------------------------------------------------
-  //
-  // $Id:$
-  //
-  // Copyright (C) 1993-1996 by id Software, Inc.
-  //
-  // This source is available for distribution and/or modification
-  // only under the terms of the DOOM Source Code License as
-  // published by id Software. All rights reserved.
-  //
-  // The source is distributed in the hope that it will be useful,
-  // but WITHOUT ANY WARRANTY; without even the implied warranty of
-  // FITNESS FOR A PARTICULAR PURPOSE. See the DOOM Source Code License
-  // for more details.
-  //
-  // DESCRIPTION:
-  //	Printed strings for translation.
-  //	English language support (default).
-  //
-  //-----------------------------------------------------------------------------
-  \n[\s{2}\t]
-  if (typeof __D_ENGLSH__ == "undefined"){
+//-----------------------------------------------------------------------------
+//
+// $Id:$
+//
+// Copyright (C) 1993-1996 by id Software, Inc.
+//
+// This source is available for distribution and/or modification
+// only under the terms of the DOOM Source Code License as
+// published by id Software. All rights reserved.
+//
+// The source is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// FITNESS FOR A PARTICULAR PURPOSE. See the DOOM Source Code License
+// for more details.
+//
+// DESCRIPTION:
+//	Printed strings for translation.
+//	English language support (default).
+//
+//-----------------------------------------------------------------------------
+
+if (typeof __D_ENGLSH__ == "undefined"){
   export const __D_ENGLSH__ :boolean = true;
   
   //
