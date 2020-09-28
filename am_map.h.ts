a0fb2a090ab1e5d5df890070d67dec40586d5cc6
XANADryden <46723459+XANADryden@users.noreply.github.com>
2020-09-28 11:26:46 -0500

    Update am_map.h.ts

--- am_map.h.ts
+++ am_map.h.ts
@@ -1,13 +1,13 @@
 if (typeof __AMMAP_H__ == "undefined"){
-    export const __AMMAP_H__: boolean = true;
+    globalThis.__AMMAP_H__ = true;
     
-    export const AM_MSGHEADER: bigint = (('a'<<24)+('m'<<16));
-    export const AM_MSGENTERED: bigint = (AM_MSGHEADER | ('e'<<8));
-    export const AM_MSGEXITED: bigint = (AM_MSGHEADER | ('x'<<8));
+    export const AM_MSGHEADER: number = (('a'<<24)+('m'<<16));
+    export const AM_MSGENTERED: number = (AM_MSGHEADER | ('e'<<8));
+    export const AM_MSGEXITED: number = (AM_MSGHEADER | ('x'<<8));
     
     
     // Called by main loop.
-    export function AM_Responder(ev:event_t):boolean;
+    export function AM_Responder(ev:event_t):  boolean;
     
     // Called by main loop.
     export function AM_Ticker (): void;
